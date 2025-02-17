d90a28ab2db054b3ac3dfbe04638fd8635a9e6ae
pavel <japavel20@gmail.com>
2025-02-17 23:30:02 +0600

    Refactor Sidebar component: integrate NavGroup and display nav items dynamically

--- src/App/View/Components/Layouts/Partials/Sidebar.php
+++ src/App/View/Components/Layouts/Partials/Sidebar.php
@@ -3,6 +3,7 @@
 namespace Layout\Manager\App\View\Components\Layouts\Partials;
 
 use Illuminate\View\Component;
+use Layout\Manager\Models\NavGroup;
 
 class Sidebar extends Component
 {
@@ -11,12 +12,20 @@ class Sidebar extends Component
      *
      * @return void
      */
+    // public $navGroup;
     public function __construct()
     {
         // update default value of $this->layoutConfig using session, db or config
+        // $this->navGroup = NavGroup::with(['navItems' => function ($query) {
+        //     $query->orderBy('sequence');
+        // }])->orderBy('sequence')->get();
     }
     public function render()
     {
-        return view('layout::components.layouts.partials.sidebar');
+        $navGroups = NavGroup::with(['navItems' => function ($query) {
+            $query->orderBy('position');
+        }])->orderBy('position')->get();
+
+        return view('layout::components.layouts.partials.sidebar', compact('navGroups'));
     }
 }
--- src/resources/views/components/layouts/partials/sidebar.blade.php
+++ src/resources/views/components/layouts/partials/sidebar.blade.php
@@ -1,3 +1,4 @@
+
 <div class="sidebar-area" id="sidebar-area">
     <div class="logo position-relative">
         <a href="/dashboard" class="d-block text-decoration-none position-relative">
@@ -11,57 +12,22 @@
 
     <aside id="layout-menu" class="layout-menu menu-vertical menu active" data-simplebar>
         <ul class="menu-inner">
-            <li class="menu-title small text-uppercase">
-                <span class="menu-title-text">MAIN</span>
-            </li>
-            <li class="menu-item" >
-                <a href="/dashboard" class="menu-link active">
-                    <span class="material-symbols-outlined menu-icon">dashboard</span>
-                    <span class="title">Dashboard</span>
-                </a>
-            </li>  
-            <li class="menu-title small text-uppercase">
-                <span class="menu-title-text">Masterdata</span>
-            </li>
-            <li class="menu-item" >
-                <a href="/dashboard" class="menu-link active">
-                    <span class="material-symbols-outlined menu-icon">dashboard</span>
-                    <span class="title">UoM</span>
-                </a>
-            </li> 
-            <li class="menu-item">
-                <a href="javascript:void(0);" class="menu-link menu-toggle active">
-                    <span class="material-symbols-outlined menu-icon">note_stack</span>
-                    <span class="title">Front Pages</span>
-                </a>
-                <ul class="menu-sub">
+            @foreach($navGroups as $navGroup)
+                <li class="menu-title small text-uppercase">
+                    <span class="menu-title-text">{{ $navGroup->title }}</span>
+                </li>
+
+                @foreach($navGroup->navItems as $item)
                     <li class="menu-item">
-                        <a href="landing-page.html" class="menu-link">
-                            Home
+                        <a href="{{ $item->url }}" class="menu-link">
+                            @if($item->icon)
+                                <span class="material-symbols-outlined menu-icon">{{ $item->icon }}</span>
+                            @endif
+                            <span class="title">{{ $item->title }}</span>
                         </a>
                     </li>
-                    <li class="menu-item">
-                        <a href="features.html" class="menu-link">
-                            Features
-                        </a>
-                    </li>
-                    <li class="menu-item">
-                        <a href="our-team.html" class="menu-link">
-                            Our Team
-                        </a>
-                    </li>
-                    <li class="menu-item">
-                        <a href="faqs.html" class="menu-link">
-                            FAQ’s
-                        </a>
-                    </li>
-                    <li class="menu-item">
-                        <a href="contact.html" class="menu-link">
-                            Contact
-                        </a>
-                    </li>
-                </ul>
-            </li>
+                @endforeach
+            @endforeach
         </ul>
 
         
