0a479c3a0821af8d7585de858def40453a824df3
pavel <japavel20@gmail.com>
2025-02-18 18:56:55 +0600

    Add core JS files: jQuery, Select2, and Bootstrap Multiselect to layout

--- src/resources/views/components/layouts/libs/js.blade.php
+++ src/resources/views/components/layouts/libs/js.blade.php
@@ -2,5 +2,9 @@
     @foreach($jsFiles as $jsFile)
     <script src="{{ $jsFile }}"></script>
     @endforeach
+    <!-- Core JS files -->
+    <script id="jquery" src="{{ asset('/themes') }}/global_assets/js/main/jquery.min.js"></script>
+    <script id="select2" src="{{ asset('/themes') }}/global_assets/js/main/select2.min.js"></script>
+    <script id="bootstrap_multiselect" src="{{ asset('/themes') }}/global_assets/js/main/bootstrap_multiselect.js"></script>
     @stack('js')
 </div>
