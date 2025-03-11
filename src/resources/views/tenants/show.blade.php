32a74827f2779c5cec715c08a6215aa2e6ddcd02
pavel <japavel20@gmail.com>
2025-03-11 22:24:34 +0600

    Update tenant status display and modify tenant details in views

--- src/resources/views/tenants/show.blade.php
+++ src/resources/views/tenants/show.blade.php
@@ -1,9 +1,9 @@
 <x-theme-master>
     <div class="card bg-white border-0 rounded-3 mb-4">
         <div class="card-body p-4">
-			<p><b>{{ __('Title') }} : </b> {{ $navItem->title }}</p>
-			<p><b>{{ __('Icon') }} : </b> {{ $navItem->icon }}</p>
-			<p><b>{{ __('Position') }} : </b> {{ $navItem->position }}</p>
+			<p><b>{{ __('Title') }} : </b> {{ $tenant->name }}</p>
+			<p><b>{{ __('Icon') }} : </b> {{ $tenant->domain }}</p>
+			<p><b>{{ __('Position') }} : </b> {{ $tenant->database }}</p>
         </div>
     </div>
 @push('css')
