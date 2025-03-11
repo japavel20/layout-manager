48818bfef14af8b1d6a3f1f4a30e7fa491d85af9
pavel <japavel20@gmail.com>
2025-03-11 21:56:24 +0600

    Fix typo in 'Add New Tenant' button label

--- src/resources/views/tenants/index.blade.php
+++ src/resources/views/tenants/index.blade.php
@@ -26,7 +26,7 @@
                 <a href="{{route('tenants.create')}}" class="btn btn-outline-primary py-1 px-2 px-sm-4 fs-14 fw-medium rounded-3 hover-bg">
                     <span class="py-sm-1 d-block">
                         <i class="ri-add-line d-none d-sm-inline-block fs-18"></i>
-                        <span>Add New Tenat</span>
+                        <span>Add New Tenant</span>
                     </span>
                 </a>
             </div> 
