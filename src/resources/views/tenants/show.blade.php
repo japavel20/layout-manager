92021804f13c5a1892a27e1294ba88b9211fb05d
japavel20 <japavel20@gmail.com>
2025-03-12 12:19:02 +0600

    Display tenant users in detail view with a structured table layout

--- src/resources/views/tenants/show.blade.php
+++ src/resources/views/tenants/show.blade.php
@@ -4,13 +4,37 @@
 			<p><b>{{ __('Title') }} : </b> {{ $tenant->name }}</p>
 			<p><b>{{ __('Icon') }} : </b> {{ $tenant->domain }}</p>
 			<p><b>{{ __('Position') }} : </b> {{ $tenant->database }}</p>
+        
+            @if(count($tenant->users))
+
+            <h3 class="mt-4 text-center text-uppercase mb-3"><strong>{{ __('Users') }}</strong></h3>
+
+            <table class="table">
+                <thead class="bg-teal-800">
+                    <tr>
+                    <th class="text-center">{{ __('Name') }}</th>
+                    <th class="text-center">{{ __('Email') }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($tenant->users as $user)
+                    <tr>
+                        <td class="text-center">{{ $user->name }}</td>
+                        <td class="text-center">{{ $user->email }}</td>
+
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            @endif
         </div>
     </div>
-@push('css')
-{{--pagespecific-css--}}
-@endpush
+    
+    @push('css')
+    {{--pagespecific-css--}}
+    @endpush
 
-@push('js')
-{{--pagespecific-js--}}
-@endpush
+    @push('js')
+    {{--pagespecific-js--}}
+    @endpush
 </x-theme-master>
